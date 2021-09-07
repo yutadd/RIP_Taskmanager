660cb311d2fd1c209119bd9487cca53fb5c2f592
yutadd <spyk4m49@gmail.com>
2021-09-07 11:41:25 +0900

    無駄に最適化

--- lock_cpu.cpp
+++ lock_cpu.cpp
@@ -1,38 +1,21 @@
 ﻿// lock_cpu.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
-
-#include <iostream>
 #include <windows.h>
-#include <time.h>
 #include <thread>
-#include <vector>
-
-static HANDLE handle = 0;
 static int end_f[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-static int i = 0;
-static int mode = 3;
 DWORD WINAPI  high_load(LPVOID id) {
     while (true) {
-        for (i= 0; end_f[(int)id] == 0; i++);
+        while(end_f[(int)id] == 0);
         Sleep(16);
     }
     return 0;
 }
-void normal_th() {
-    for (i = 0; end_f == 0; i++);
-}
 int main()
 {
+    HANDLE handle = 0;
     int ia = 0;
-    
         for (ia = 0; ia < 16; ia++) {
-            handle = CreateThread(
-                0,
-                0,
-                &high_load,
-                (LPVOID)(ia),
-                0,
-                0);
+            handle = CreateThread(0,0,&high_load,(LPVOID)(ia),0,0);
             SetThreadAffinityMask(handle, 1 << (ia));
             end_f[(ia)] = 0;
         }
@@ -46,8 +29,6 @@ int main()
                 end_f[ia] = 0;
             }
         }
-    
-    Sleep(40000);
     //Sleep(1);
     //printf("%d", i);
     //Benched for 30sec.
