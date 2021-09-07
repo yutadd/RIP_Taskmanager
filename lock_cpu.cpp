d64acbe029902f32b5b27625b996318d810a15a7
yutadd <spyk4m49@gmail.com>
2021-09-07 10:34:23 +0900

    RIP_TaskManager

--- lock_cpu.cpp
+++ lock_cpu.cpp
@@ -7,11 +7,14 @@
 #include <thread>
 
 static HANDLE handle = 0;
-static int end_f = 0;
+static int end_f[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 static int i = 0;
 static int mode = 3;
 DWORD WINAPI  high_load(LPVOID id) {
-    for (i= 0; end_f == 0; i++);
+    while (true) {
+        for (i= 0; end_f[(int)id] == 0; i++);
+        Sleep(16);
+    }
     return 0;
 }
 void normal_th() {
@@ -19,28 +22,21 @@ void normal_th() {
 }
 int main()
 {
-    if (mode == 0) {
-        handle = CreateThread(0, 0, &high_load, 0, 0, 0);
-        SetThreadAffinityMask(handle, 1 << 0);
+    while (true) {
+        for (int i = 0; i < 16; i++) {
+            handle = CreateThread(0, 0, &high_load, (LPVOID)(i), 0, 0);
+            SetThreadAffinityMask(handle, 1 << (i));
+            end_f[(i)] = 0;
+        }
+        Sleep(310);
+        for (int i = 0; i < 16; i++) {
+            end_f[i] = 1;
+        }
+        Sleep(520);
     }
-    else if(mode==1) {
-        handle = CreateThread(0, 0, &high_load, 0, 0, 0);
-    }
-    else if (mode == 2) {
-        std::thread th_1(normal_th);
-        SetThreadAffinityMask(th_1.native_handle(), 1 << 0);
-        th_1.detach();
-    }
-    else if (mode == 3) {
-        std::thread th_2(normal_th);
-        th_2.detach();
-    }else {
-        std::cout << "Please set valid mode." << std::endl;
-    }
-    Sleep(30000);//WaitForSingleObject(handle[0], INFINITE);
-    end_f = 1;
-    Sleep(1);
-    printf("%d", i);
+    Sleep(40000);
+    //Sleep(1);
+    //printf("%d", i);
     //Benched for 30sec.
     //lock thread to core:    267161689
     //lock free thread:       822861222
